f22d7fd1bbd993337e4e093e8e9ce8a161c1e54f
VTG <varastgergo2004@gmail.com>
2024-05-29 18:40:54 +0200

    kész

--- cucc/View/DataView.xaml.cs
+++ cucc/View/DataView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Content;
+using DB.Worker;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,69 @@ namespace cucc.View
     /// </summary>
     public partial class DataView : UserControl
     {
+        public readonly Context _context;
+        public User _selectedUser;
+
         public DataView()
         {
             InitializeComponent();
+            _context = new Context();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            var users = _context.User.ToList();
+            listBox.ItemsSource = users;
+        }
+
+        public void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedIndex != -1)
+            {
+                _selectedUser = (User)listBox.SelectedItem;
+                nameTextBox.Text = _selectedUser.Name;
+                ageTextBox.Text = _selectedUser.Age;
+                cityTextBox.Text = _selectedUser.City;
+                jobTextBox.Text = _selectedUser.Job;
+                incomeTextBox.Text = _selectedUser.Income;
+            }
+        }
+
+        public async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedUser != null)
+            {
+                _selectedUser.Name = nameTextBox.Text;
+                _selectedUser.Age = ageTextBox.Text;
+                _selectedUser.City = cityTextBox.Text;
+                _selectedUser.Job = jobTextBox.Text;
+                _selectedUser.Income = incomeTextBox.Text;
+
+                _context.User.Update(_selectedUser);
+                await _context.SaveChangesAsync();
+                LoadData();
+            }
+        }
+
+        public async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedUser != null)
+            {
+                _context.User.Remove(_selectedUser);
+                await _context.SaveChangesAsync();
+                LoadData();
+                ClearFields();
+            }
+        }
+
+        public void ClearFields()
+        {
+            nameTextBox.Text = "";
+            ageTextBox.Text = "";
+            cityTextBox.Text = "";
+            jobTextBox.Text = "";
+            incomeTextBox.Text = "";
         }
     }
 }
